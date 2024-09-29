c02f301ac834372eff41a8bcf0102397756fe7a6
aayush <159913583+aayushhks@users.noreply.github.com>
2024-09-30 00:41:21 +0530

    Update routes.js

--- iitk-core-frontend/src/routes.js
+++ iitk-core-frontend/src/routes.js
@@ -13,11 +13,11 @@ const optedSurveys = React.lazy(() => import('./views/base/opted-surveys/opted-s
 // const Breadcrumbs = React.lazy(() => import('./views/base/breadcrumbs/Breadcrumbs'))
 const Cards = React.lazy(() => import('./views/base/cards/Cards'))
 // const Carousels = React.lazy(() => import('./views/base/carousels/Carousels'))
-const Collapses = React.lazy(() => import('./views/base/collapses/Collapses'))
+// const Collapses = React.lazy(() => import('./views/base/collapses/Collapses'))
 const ListGroups = React.lazy(() => import('./views/base/list-groups/ListGroups'))
-const Navs = React.lazy(() => import('./views/base/navs/Navs'))
+// const Navs = React.lazy(() => import('./views/base/navs/Navs'))
 // const Paginations = React.lazy(() => import('./views/base/paginations/Paginations'))
-const Placeholders = React.lazy(() => import('./views/base/placeholders/Placeholders'))
+// const Placeholders = React.lazy(() => import('./views/base/placeholders/Placeholders'))
 // const Popovers = React.lazy(() => import('./views/base/popovers/Popovers'))
 // const Progress = React.lazy(() => import('./views/base/progress/Progress'))
 const Spinners = React.lazy(() => import('./views/base/spinners/Spinners'))
@@ -71,11 +71,11 @@ const routes = [
   // { path: '/base/breadcrumbs', name: 'Breadcrumbs', element: Breadcrumbs },
   { path: '/base/cards', name: 'Cards', element: Cards },
   // { path: '/base/carousels', name: 'Carousel', element: Carousels },
-  { path: '/base/collapses', name: 'Collapse', element: Collapses },
+  // { path: '/base/collapses', name: 'Collapse', element: Collapses },
   { path: '/base/list-groups', name: 'List Groups', element: ListGroups },
-  { path: '/base/navs', name: 'Navs', element: Navs },
+  // { path: '/base/navs', name: 'Navs', element: Navs },
   // { path: '/base/paginations', name: 'Paginations', element: Paginations },
-  { path: '/base/placeholders', name: 'Placeholders', element: Placeholders },
+  // { path: '/base/placeholders', name: 'Placeholders', element: Placeholders },
   // { path: '/base/popovers', name: 'Popovers', element: Popovers },
   // { path: '/base/progress', name: 'Progress', element: Progress },
   { path: '/base/spinners', name: 'Spinners', element: Spinners },
