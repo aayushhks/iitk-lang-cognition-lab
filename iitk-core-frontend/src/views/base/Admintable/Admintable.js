8ca7b10aa6ee445bd93137bc6a5230250c702359
aayush <159913583+aayushhks@users.noreply.github.com>
2024-09-29 19:02:38 +0530

    Update Admintable.js

--- iitk-core-frontend/src/views/base/Admintable/Admintable.js
+++ iitk-core-frontend/src/views/base/Admintable/Admintable.js
@@ -19,7 +19,7 @@ import {
   CFormInput,
   CFormLabel
 } from '@coreui/react'
-import { DocsExample } from 'src/components'
+// import { DocsExample } from 'src/components'
 
 const AdMgmtTables = () => {
   const endOffset = 1000; // flask call
@@ -53,7 +53,7 @@ const AdMgmtTables = () => {
 
   const get_ad_mgmt_table = async () => {
     try {
-      const endpoint = `http://127.0.0.1:4997/get-ad-mgmt-table`;
+      const endpoint = `http://10.162.20.250:4997/get-ad-mgmt-table`;
       const response = await fetch(endpoint, {
         method: 'POST',
         headers: {
@@ -114,14 +114,6 @@ const AdMgmtTables = () => {
   useEffect(() => {
     // Call get_users_data initially
     get_ad_mgmt_table();
-
-    // // Set up the interval to call get_users_data every 500ms
-    // const intervalId = setInterval(() => {
-    //   get_ad_mgmt_table();
-    // }, 5000); // Every 15 seconds (data too big to fetch in time)
-
-    // Cleanup function to clear the interval when the component unmounts
-    // return () => clearInterval(intervalId);
   }, []); // Empty dependency array means this effect runs once when the component mounts
 
   useEffect(() => {
@@ -142,7 +134,7 @@ const AdMgmtTables = () => {
   const handleEdit = async (email) => {
     // Call the edit function from the API
     try {
-      const response = await fetch(`http://127.0.0.1:4997/edit-user-account`, {
+      const response = await fetch(`http://10.162.20.250:4997/edit-user-account`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -163,7 +155,7 @@ const AdMgmtTables = () => {
   const handleDelete = async (email) => {
     // Call the edit function from the API
     try {
-      const response = await fetch(`http://127.0.0.1:4997/delete-user-account`, {
+      const response = await fetch(`http://10.162.20.250:4997/delete-user-account`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -191,9 +183,13 @@ const AdMgmtTables = () => {
 
     if (Object.keys(adMgmtData).length === 0) {
       return (
-        <div className="no-recs">
-          <h5>No records found</h5>
-        </div>
+        <CTableRow>
+          <CTableDataCell colSpan={4} className="no-recs-cell-admintable">
+            <div className="no-recs-admintable">
+              <h5>No records found</h5>
+            </div>
+          </CTableDataCell>
+        </CTableRow>
       );
     } else {
       return (
@@ -221,38 +217,13 @@ const AdMgmtTables = () => {
     get_ad_mgmt_table()
   }, [filterQuery, filterType])
 
-  // useEffect(() => {
-  //   const handleSearch = async () => {
-  //     try {
-  //       const response = await axios.post('/get-ad-mgmt-table', {
-  //         offset_start: 0,  // Adjust offset as needed
-  //         offset_end: 100,  // Adjust offset as needed
-  //         filter_type: filterType,
-  //         filter_query: searchQuery,
-  //       });
-
-  //       if (response.data.success) {
-  //         setResults(response.data.data);
-  //         // Handle the returned data
-  //       } else {
-  //         console.error('Error:', response.data.msg);
-  //       }
-  //     } catch (error) {
-  //       console.error('Error fetching data:', error);
-  //     }
-  //   };
-
-  //   // Call the search function when either searchQuery or filterType changes
-  //   handleSearch();
-  // }, [searchQuery, filterType]);
 
   return (
-    // <p>Treat</p>
     <CRow>
       <CCol xs={12}>
         <CCard className="mb-4">
           <CCardHeader>
-            <strong>React Table</strong> <small>Basic example</small>
+            <strong>Admin Table</strong>
           </CCardHeader>
           <CCardBody>
             <div className="page-buttons">
@@ -280,38 +251,14 @@ const AdMgmtTables = () => {
                 <p>#Results: {filterResultCount}</p>
               </div>
             </div>
-            <DocsExample href="components/table">
+            {/* <DocsExample href="components/table"> */}
             <CTable align="middle" responsive>
             <CTableBody>
               {header()}
               {(Object.keys(adMgmtData).length !== 0) ? rows : null}
-              {/* {rows.length ? rows : <CTableRow><CTableDataCell colSpan="10" className="text-center">No records found</CTableDataCell></CTableRow>} */}
-              {/* <CTableRow>
-                {
-                  adMgmtData.map((item, index) => (
-                    <CTableData key={index}>
-                      <CTableCell>{item.email}</CTableCell>
-                    </CTableData>
-                  ))
-                }
-                <CTableDataCell>
-                  MAKIchew
-                </CTableDataCell>
-              </CTableRow>
-              <CTableRow align="bottom">
-              </CTableRow>
-              <CTableRow>
-                <CTableDataCell>
-                  This cell inherits <code>vertical-align: middle;</code> from the table
-                </CTableDataCell>
-                <CTableDataCell>
-                  hehe lol <code>colorful</code> from the table
-                </CTableDataCell>
-                <CTableDataCell align="top">This cell is aligned to the top.</CTableDataCell>
-              </CTableRow> */}
+
             </CTableBody>
           </CTable>
-          </DocsExample>
           </CCardBody>
         </CCard>
       </CCol>
@@ -319,4 +266,4 @@ const AdMgmtTables = () => {
   )
 }
 
-export default AdMgmtTables
+export default AdMgmtTables;
