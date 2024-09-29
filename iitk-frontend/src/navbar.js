f147542de7ee2083cc26c3357c7f2d5f79eb6376
Vivo Jay <67545205+Vivojay@users.noreply.github.com>
2024-09-29 18:42:06 +0530

    Update navbar.js

--- iitk-frontend/src/navbar.js
+++ iitk-frontend/src/navbar.js
@@ -1,12 +1,15 @@
 import React, { useState, useEffect } from 'react';
-import iitlogo from './assets/logo.png';
+import manipalLogo from './assets/manipal.png';
 // import './navbar.css';
 import { useNavigate } from 'react-router-dom';
 
+import { ip } from './config'
+
 function Navbar(props) {
     const {
-        isSignupPage,
         isLoginPage,
+        isSignupPage,
+        isMyProfilePage,
         homeIsDisabled,
         profileIsDisabled,
         myProjectsIsDisabled,
@@ -15,23 +18,51 @@ function Navbar(props) {
 
     const [theme, setTheme] = useState('light'); // Default theme is light
     const [signUpOutText, setSignUpOutText] = useState(true);
+    // const iitlogo = "https://jaipur.manipal.edu/img/manipal-university-jaipur-logo.svg";
+
+    const update_users_online = async () => {
+        const email_rem = localStorage.getItem("email");
+        try {
+            const response = await fetch(`${ip[0]}/update-online-users`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ "email_rem": email_rem }),
+            });
+
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+
+            const result = await response.json();
+            // console.log(result);
+            // setOnlineUsers(result);
+        } catch (error) {
+            console.log('An error occurred:', error);
+        }
+    }
 
     useEffect(() => {
         // console.log(signUpOutText, isSignupPage); // debug
 
         if (signUpOutText === 'Log in') {
             setSignUpOutText('Sign up');
-            if (localStorage.length !== 0) {
+            if (!(localStorage.length === 0 || JSON.parse(localStorage.userinfo)['isadmin'])) {
                 navigate('/home');
             }
         }
         if (isSignupPage) {
             setSignUpOutText("Log in");
-            if (localStorage.length !== 0) {
+            if (!(localStorage.length === 0 || JSON.parse(localStorage.userinfo)['isadmin'])) {
                 navigate('/home');
             }
-        } else {
-            if (localStorage.length === 0) {
+        }
+        else {
+            if (localStorage.length === 0 || JSON.parse(localStorage.userinfo)['isadmin']) {
+                if (isMyProfilePage) {
+                    navigate('/login');
+                }
                 setSignUpOutText("Sign up");
             } else {
                 setSignUpOutText("Sign out");
@@ -85,11 +116,12 @@ function Navbar(props) {
             if (isSignupPage) {
                 setSignUpOutText("Log in");
             } else {
-                if (localStorage.length === 0) {
+                if (localStorage.length === 0 || JSON.parse(localStorage.userinfo)['isadmin']) {
                     setSignUpOutText("Sign up");
                     navigate('/signup');
                 } else {
                     setSignUpOutText("Sign out");
+                    update_users_online();
                     localStorage.clear();
                     navigate('/login');
                 }
@@ -101,7 +133,7 @@ function Navbar(props) {
         <nav className={`navbar navbar-expand-lg ${theme == "dark" ? "navbar-dark bg-dark" : "navbar-light bg-light"} navbarr`}>
             <div className="container-fluid">
                 <div className="d-flex align-items-center">
-                    <img src={iitlogo} alt="IIT Logo" className="navbar-logo" />
+                    <img src={manipalLogo} alt="Manipal Logo" className="navbar-logo" />
                     <a className="navbar-brand" href="/">Language Cognition Lab</a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
