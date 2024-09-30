d94ec293b63dbac7b7989272f078c49cc99cf505
aayush <159913583+aayushhks@users.noreply.github.com>
2024-10-01 01:37:47 +0530

    Update AppHeaderDropdown.js

--- iitk-core-frontend/src/components/header/AppHeaderDropdown.js
+++ iitk-core-frontend/src/components/header/AppHeaderDropdown.js
@@ -23,25 +23,64 @@ import {
 import CIcon from '@coreui/icons-react'
 
 import avatar8 from './../../assets/images/avatars/8.jpg'
+// import userIcon from './../../assets/images/avatars/user-icon.png'
+import { ip } from '../../../../../iitk2/lang-cognition-lab/iitk-frontend/src/config.js';
 
 const AppHeaderDropdown = () => {
+
+  const handleAdminLogout = async () => {
+    // Admin Logout
+    localStorage.removeItem('email');
+    localStorage.removeItem('auth_token');
+
+    try {
+      const endpoint = `${ip[0]}/logout`;
+      const email = "admin@admin.com";
+
+      const response = await fetch(endpoint, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email })
+      });
+
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+
+      const result = await response.json();
+      console.log(result);
+
+      if (result.success === true) {
+        const data = result.data;
+        window.location.href = `${ip[2]}/login`;
+        // setAdminLoggedIn(data);
+      } else {
+        console.log("Error: ", result.err);
+      }
+    } catch (error) {
+      console.log('An error occurred:', error);
+    }
+  }
+
   return (
     <CDropdown variant="nav-item">
       <CDropdownToggle placement="bottom-end" className="py-0 pe-0" caret={false}>
         <CAvatar src={avatar8} size="md" />
       </CDropdownToggle>
       <CDropdownMenu className="pt-0" placement="bottom-end">
-        <CDropdownHeader className="bg-body-secondary fw-semibold mb-2">Account</CDropdownHeader>
+        {/* <CDropdownHeader className="bg-body-secondary fw-semibold mb-2">Account</CDropdownHeader>
         <CDropdownItem href="#">
           <CIcon icon={cilBell} className="me-2" />
-          Updates
+            Updates
           <CBadge color="info" className="ms-2">
             42
           </CBadge>
         </CDropdownItem>
         <CDropdownItem href="#">
           <CIcon icon={cilEnvelopeOpen} className="me-2" />
-          Messages
+            Messages
           <CBadge color="success" className="ms-2">
             42
           </CBadge>
@@ -71,20 +110,20 @@ const AppHeaderDropdown = () => {
         </CDropdownItem>
         <CDropdownItem href="#">
           <CIcon icon={cilCreditCard} className="me-2" />
-          Payments
+            Payments
           <CBadge color="secondary" className="ms-2">
             42
           </CBadge>
         </CDropdownItem>
         <CDropdownItem href="#">
           <CIcon icon={cilFile} className="me-2" />
-          Projects
+            Projects
           <CBadge color="primary" className="ms-2">
             42
           </CBadge>
-        </CDropdownItem>
+        </CDropdownItem> */}
         <CDropdownDivider />
-        <CDropdownItem href="#">
+        <CDropdownItem onClick={handleAdminLogout}>
           <CIcon icon={cilLockLocked} className="me-2" />
           Lock Account
         </CDropdownItem>
@@ -93,4 +132,4 @@ const AppHeaderDropdown = () => {
   )
 }
 
-export default AppHeaderDropdown
+export default AppHeaderDropdown;
